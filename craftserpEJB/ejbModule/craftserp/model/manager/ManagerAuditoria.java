008137c70f5200ad74bae0bfb24a601c0256ee3c
Ruth Huertas <alejahuertas1417@gmail.com>
2020-02-13 06:02:16 -0500

    Reportes

---
+++ craftserpEJB/ejbModule/craftserp/model/manager/ManagerAuditoria.java
@@ -0,0 +1,20 @@
+package craftserp.model.manager;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class ManagerAuditoria
+ */
+@Stateless
+@LocalBean
+public class ManagerAuditoria {
+
+    /**
+     * Default constructor. 
+     */
+    public ManagerAuditoria() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
